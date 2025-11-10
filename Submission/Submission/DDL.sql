b5c6156a61a57846cf654bc1d2ff52fa48b3b7dd
N0va294 <michaeldmaclean11@gmail.com>
2025-11-09 20:16:39 -0500

    Delete Submission directory

--- Submission/Submission/DDL.sql
+++
@@ -1,7 +0,0 @@
-create table Students(
-    student_id serial primary key,
-    first_name text not null,
-    last_name text not null,
-    email text unique not null,
-    enrollment_date date
-);
